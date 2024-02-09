3e6cf74ce8c80334670316cd9dd1a32540284a54
Jon <jonathan.k.wyatt@gmail.com>
2024-02-08 22:43:49 -0800

    meta progression and menus

--- scenes/UI/main_menu.gd
+++ scenes/UI/main_menu.gd
@@ -4,6 +4,7 @@ var options_scene = preload("res://scenes/UI/options_menu.tscn")
 
 func _ready():
 	%PlayButton.pressed.connect(on_play_pressed)
+	%UpgradesButton.pressed.connect(on_upgrades_pressed)
 	%OptionsButton.pressed.connect(on_options_pressed)
 	%QuitButton.pressed.connect(on_quit_pressed)
 	
@@ -12,7 +13,13 @@ func on_play_pressed():
 	ScreenTransition.transition()
 	await ScreenTransition.transition_halfway
 	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
-	
+
+
+func on_upgrades_pressed():
+	ScreenTransition.transition()
+	await ScreenTransition.transition_halfway
+	get_tree().change_scene_to_file("res://scenes/UI/meta_menu.tscn")
+
 
 func on_options_pressed():
 	ScreenTransition.transition()
--- scenes/autoload/meta_progression.gd
+++ scenes/autoload/meta_progression.gd
@@ -1,13 +1,29 @@
 extends Node
 
+const SAVE_FILE_PATH = "user://game.save"
+
 var save_data: Dictionary = {
 	"meta_upgrade_currency": 0,
 	"meta_upgrades": {}
 }
 
+
 func _ready():
 	GameEvents.experience_vial_collected.connect(on_experience_collected)
-	
+	load_save_file()
+
+
+func load_save_file():
+	if !FileAccess.file_exists(SAVE_FILE_PATH):
+		return
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.READ)
+	save_data = file.get_var()
+
+
+func save():
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE)
+	file.store_var(save_data)
+
 
 func add_meta_upgrade(upgrade: MetaUpgrade):
 	if !save_data["meta_upgrades"].has(upgrade.id):
@@ -16,6 +32,13 @@ func add_meta_upgrade(upgrade: MetaUpgrade):
 		}
 		
 	save_data["meta_upgrades"][upgrade.id]["quantity"] += 1
+	save()
+
+
+func get_upgrade_count(upgrade_id: String):
+	if MetaProgression.save_data["meta_upgrades"].has(upgrade_id):
+		return save_data["meta_upgrades"][upgrade_id]["quantity"]
+	return 0
 
 
 func on_experience_collected(number):
